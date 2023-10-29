8f77060e7551bdacee2603bf72eded05703387ac
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-10-29 20:14:55 +0530

    Add files via upload

---
+++ sum and average.py
@@ -0,0 +1,10 @@
+#Calculate the sum and average of first natural number :
+
+
+n=int(input("enter the natural number:"))
+
+sum_n=(n*(n+1))//2
+avg_n=sum_n/n
+
+print("the sum of the natural number is :",sum_n)
+print("the average of the natural number is :",avg_n) 
