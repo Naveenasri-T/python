8b4861a5bee7805b63cb5c5281280c4fa30d28dd
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-10-29 20:12:01 +0530

    Add files via upload

---
+++ largest of number.py
@@ -0,0 +1,8 @@
+#find the larger number of two numbers:
+ 
+a= int(input("enter the number a:"))
+b=int(input("enter the number b:"))
+if a>b:
+ print("a is a larger number",a)
+else:
+ print("b is a larger number",b) 
