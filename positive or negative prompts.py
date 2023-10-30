c250b7e086bfe93a727c3365ed513edee709e6ad
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-10-30 06:02:13 +0530

    Add files via upload

---
+++ positive or negative prompts.py
@@ -0,0 +1,9 @@
+#prompts the user to enter a number and print the intervals:
+num=int(input("enter the number:"))
+if num<0:
+
+ print("the intervel is negative")
+elif num==0:
+ print("the interval is zero")
+else:
+ print("the interval is positive") 
