6f65ca3f2e6521f99d8030dba969de1af03d97f3
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-01 19:24:44 +0530

    Add files via upload

---
+++ ThreeDigitOr_Not.py
@@ -0,0 +1,8 @@
+#check weather the given number is three digit number or not:
+num=int(input("Enter the number:"))
+if (num>=100)and (num<=999):
+    print("the givin number is three digit number",num)
+
+else:
+
+    print(" Not a three digit Number") 
