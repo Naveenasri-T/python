742de27c2cba44a251208e5dfb346cd44eb75d72
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-02 17:59:15 +0530

    Add files via upload

---
+++ volume of cuboid.py
@@ -0,0 +1,10 @@
+#volume of cuboid :
+def volume_calc (w,h,l):
+ return w*h*l
+
+w= int(input("enter the value:"))
+l= int(input("enter the value:"))
+h= int(input("enter the value:"))
+
+
+print(volume_calc(w,h,l)) 
