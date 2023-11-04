0807134753bb9d9d6e28848b07b9d9c147b5f4c1
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-04 18:53:55 +0530

    Add files via upload

---
+++ list appending.py
@@ -0,0 +1,7 @@
+#list appending
+n=int(input("Enter the number of element from the list:"))
+list=[]
+for i in range(n):
+    your_list=int(input("enter your list element:"))
+    list.append(your_list)
+print("list element are:",list)
