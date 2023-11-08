207ba80e29a4bc85affda546aae64d3ea1345d22
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-08 20:22:51 +0530

    Add files via upload

---
+++ simple intrest calculation.py
@@ -0,0 +1,6 @@
+#simple intrest calculation
+principal= float(input("Enter your principal amount:"))
+rate = float(input("Enter your rate of intrest:"))
+time = float(input("Enter the time period:"))
+simple_intrest=(principal*rate*time)//100
+print("simple intrest=",simple_intrest)
