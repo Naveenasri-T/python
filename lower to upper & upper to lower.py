33042ad1340b4fcd46f4bf97c19ac74bc3ddc6e6
NAVEENASRI T <118744037+Naveenasri-T@users.noreply.github.com>
2023-11-20 18:36:00 +0530

    Add files via upload

---
+++ lower to upper & upper to lower.py
@@ -0,0 +1,12 @@
+#Converting upper to lower &lower to upper str:
+ch=input("enter the character:")
+
+if ch.islower():
+
+ print("the character is lowercase")
+ print("converting to uppercase",ch.upper())
+ 
+else:
+ 
+ print("the character is uppercase")
+ print("converting the lowercase",ch.lower())
